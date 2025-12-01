a6e89230be846e129c90dede18afe30f497e5f6e
Snowy4Everr <165119038+Snowy4Everr@users.noreply.github.com>
2025-12-01 14:27:15 +0000

    2d lists video

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,3 +1,25 @@
+# Nested List Video Example:
+fruits =     ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats =      ["chicken", "fish", "turkey"]
+
+groceries = [["apple", "orange", "banana", "coconut"],
+              ["celery", "carrots", "potatoes"],
+                ["chicken", "fish", "turkey"]]
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
+    print()
+
+# 2D Tupple
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
 # Objective:
 # Students will manipulate nested lists and understand basic list comprehensions.
 
